089f1f6239fcbfbf2aa29cfce860c9eb07e12884
zeke <alexguimaraes95@gmail.com>
2020-04-07 23:21:38 +0100

    Hello Git

---
+++ app/src/main/java/dam_A42346/thematiccalculator/Calculator.kt
@@ -0,0 +1,29 @@
+package dam_A42346.thematiccalculator
+
+import kotlin.math.pow
+
+class Calculator {
+    fun add(augend: Double, addend: Double): Double {
+        return augend + addend
+    }
+
+    fun sub(minuend: Double, subtrahend: Double): Double {
+        return minuend - subtrahend
+    }
+
+    fun mult(multiplier: Double, multiplicand: Double): Double {
+        return multiplier * multiplicand
+    }
+
+    fun div(dividen: Double, divisor: Double): Double {
+        return dividen / divisor
+    }
+
+    fun root(base: Double, value: Double): Double{
+        return base.pow(1/value)
+    }
+
+    fun pow(base: Double, value:Double):Double{
+        return base.pow(value)
+    }
+}
---
+++ app/src/main/java/dam_A42346/thematiccalculator/MainActivity.kt
@@ -0,0 +1,166 @@
+package dam_A42346.thematiccalculator
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mem: Double = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val plus: Button = this.findViewById(R.id.plus)
+        plus.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        plus.setOnClickListener(this)
+        val minus: Button = this.findViewById(R.id.minus)
+        minus.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        minus.setOnClickListener(this)
+        val times: Button = this.findViewById(R.id.times)
+        times.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        times.setOnClickListener(this)
+        val divide: Button = this.findViewById(R.id.divide)
+        divide.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        divide.setOnClickListener(this)
+        val root: Button = this.findViewById(R.id.root)
+        root.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        root.setOnClickListener(this)
+        val pow: Button = this.findViewById(R.id.pow)
+        pow.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        pow.setOnClickListener(this)
+        val setMem: Button = this.findViewById(R.id.setMem)
+        setMem.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        setMem.setOnClickListener(this)
+        val getMem: Button = this.findViewById(R.id.getMem)
+        getMem.setBackgroundColor(ContextCompat.getColor(baseContext, R.color.colorWhite))
+        getMem.setOnClickListener(this)
+
+        val winIcon: ImageButton = findViewById(R.id.winIcon)
+        winIcon.setOnTouchListener { arg0, arg1 ->
+            setContext(R.id.winIcon)
+            false
+        }
+        val macIcon: ImageButton = findViewById(R.id.macIcon)
+        macIcon.setOnTouchListener { arg0, arg1 ->
+            setContext(R.id.macIcon)
+            false
+        }
+        val linuxIcon: ImageButton = findViewById(R.id.linuxIcon)
+        linuxIcon.setOnTouchListener { v, event ->
+            setContext(R.id.linuxIcon)
+            false
+        }
+    }
+
+
+    private fun setContext(id: Int) {
+
+        var fc = 0
+        val container: View = findViewById(R.id.container)
+        val bc = R.color.colorWhite
+
+        when (id) {
+
+            R.id.winIcon -> {
+                container.setBackgroundResource(R.drawable.windowswallpaper)
+                fc = R.color.colorWindows
+                //          bc = R.color.fcpb;
+            }
+            R.id.linuxIcon -> {
+                container.setBackgroundResource(R.drawable.ubuntuwallpaper)
+                fc = R.color.colorLinux
+                //          bc = R.color.fcpb;
+            }
+            R.id.macIcon -> {
+                container.setBackgroundResource(R.drawable.macwallpaper)
+                fc = R.color.colorMacOS
+                //          bc = R.color.fcpb;
+            }
+        }
+        val title: TextView = findViewById(R.id.selectTeam)
+        title.setTextColor(ContextCompat.getColor(baseContext, fc))
+
+        val operand1: TextView = findViewById(R.id.operand1)
+        operand1.setTextColor(ContextCompat.getColor(baseContext, fc))
+        operand1.setBackgroundColor(ContextCompat.getColor(baseContext, bc))
+        val operand2: TextView = findViewById(R.id.operand2)
+        operand2.setTextColor(ContextCompat.getColor(baseContext, fc))
+        operand2.setBackgroundColor(ContextCompat.getColor(baseContext, bc))
+
+        val numberOp1: TextView = findViewById(R.id.numberOp1)
+        numberOp1.setTextColor(ContextCompat.getColor(baseContext, bc))
+        numberOp1.setBackgroundColor(ContextCompat.getColor(baseContext, fc))
+        val numberOp2: TextView = findViewById(R.id.numberOp2)
+        numberOp2.setTextColor(ContextCompat.getColor(baseContext, bc))
+        numberOp2.setBackgroundColor(ContextCompat.getColor(baseContext, fc))
+
+        val plus: Button = findViewById(R.id.plus)
+        plus.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val minus: Button = findViewById(R.id.minus)
+        minus.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val times: Button = findViewById(R.id.times)
+        times.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val divide: Button = findViewById(R.id.divide)
+        divide.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val root: Button = findViewById(R.id.root)
+        root.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val pow: Button = findViewById(R.id.pow)
+        pow.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val setMem: Button = findViewById(R.id.setMem)
+        setMem.setTextColor(ContextCompat.getColor(baseContext, fc))
+        val getMem: Button = findViewById(R.id.getMem)
+        getMem.setTextColor(ContextCompat.getColor(baseContext, fc))
+
+    }
+
+    override fun onClick(v: View) {
+
+        val c = Calculator()
+        val b = v as Button
+        var value = 0.0
+        val numberOp1: TextView = findViewById(R.id.numberOp1)
+        val numberOp2: TextView = findViewById(R.id.numberOp2)
+        val sNum1 = numberOp1.text.toString()
+        val sNum2 = numberOp2.text.toString()
+        var num1 = sNum1.toDouble()
+        val num2 = sNum2.toDouble()
+
+
+        when (b.id) {
+
+            R.id.plus -> {
+                value = c.add(num1, num2)
+            }
+            R.id.minus -> {
+                value = c.sub(num1, num2)
+            }
+            R.id.times -> {
+                value = c.mult(num1, num2)
+            }
+            R.id.divide -> {
+                value = c.div(num1, num2)
+            }
+            R.id.pow -> {
+               value = c.pow(num1,num2)
+            }
+            R.id.root -> {
+                value = c.root(num1,num2)
+            }
+            R.id.setMem -> {
+                mem = num1
+            }
+            R.id.getMem -> {
+                value = mem
+            }
+        }
+
+        numberOp1.text = value.toString()
+    }
+}
